037a94aa8108eb4adad9330c49bc9d2da9cf9aa6
crispynoodlesoup <thepanman321@gmail.com>
2023-02-05 01:27:43 -0600

    password matching

---
+++ app.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const button = document.querySelector(".submit")
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    if(document.getElementById("confirm-password").value != document.getElementById("password").value) {
+        alert("Your passwords do not match, please retype them");
+        document.getElementById("confirm-password").value = "";
+        document.getElementById("password").value = "";
+        e.preventDefault();
+    }
+}
